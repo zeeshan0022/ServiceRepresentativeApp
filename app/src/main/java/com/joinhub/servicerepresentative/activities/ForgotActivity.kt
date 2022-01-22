42d469776ada5b8763dc014668516742203db4e0
Zeeshan Nawaz <zeeshannn0022@gmail.com>
2022-01-22 17:15:10 +0500

    Service Representative App

--- app/src/main/java/com/joinhub/servicerepresentative/activities/ForgotActivity.kt
+++ app/src/main/java/com/joinhub/servicerepresentative/activities/ForgotActivity.kt
@@ -2,11 +2,21 @@ package com.joinhub.servicerepresentative.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.joinhub.complaintprotaluser.utilties.Constants
 import com.joinhub.servicerepresentative.R
 
 class ForgotActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot)
+    init()
+    }
+
+    private fun init(){
+        if(MainActivity.themeBool){
+            Constants.darkThemeStyle(this)
+        }else{
+            Constants.lightThemeStyle(this)
+        }
     }
 }
